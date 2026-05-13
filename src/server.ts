82c90f0cbb230710de76d2cf005c7e535bdb3cdc
accenPanda <arindam.panda@accenture.com>
2026-05-13 10:23:48 +0530

    testing env on prod

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ dotenv.config();
 
 const PORT = Number(process.env.PORT) || 4000;
 
+console.log("Env - ",process.env.RANDOM);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

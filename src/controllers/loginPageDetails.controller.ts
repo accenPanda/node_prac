7bd6c67dc778be1f22b8cd6f4087510b1fe87f2e
accenPanda <arindam.panda@accenture.com>
2026-05-23 13:33:43 +0530

    implemented 2 new testing apis

--- src/controllers/loginPageDetails.controller.ts
+++ src/controllers/loginPageDetails.controller.ts
@@ -4,3 +4,12 @@ import { loginPageDetails } from "../data/loginPageDetails";
 export const getLoginPageDetails = (_req: Request, res: Response): void => {
   res.status(200).json(loginPageDetails);
 };
+
+export const errorTest = (_req: Request, res: Response): void => {
+  throw new Error("Application Insights test error");
+};
+
+export const slowAPI = async (_req: Request, res: Response): Promise<void> => {
+    await new Promise(r => setTimeout(r, 3000));
+  res.json({ ok: true });
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getHealth } from "../controllers/health.controller";
 import { getGreeting } from "../controllers/greet.controller";
 import { checkEnv } from "../controllers/envTest.controller";
-import { getLoginPageDetails } from "../controllers/loginPageDetails.controller";
+import { errorTest, getLoginPageDetails, slowAPI } from "../controllers/loginPageDetails.controller";
 
 const router = Router();
 
@@ -10,5 +10,7 @@ router.get("/health", getHealth);
 router.get("/greet", getGreeting);
 router.get("/test-env", checkEnv);
 router.get("/loginPageDetails", getLoginPageDetails);
+router.get("/error-test", errorTest);
+router.get("/slow-api", slowAPI);
 
 export default router;
